fb7eb194e9341d4b6e63dc6ff75d5d83591476c1
Wesley Feitosa <jwesleydasilva@gmail.com>
2019-04-21 09:29:17 -0300

    Primeiro commit com a estrutura básica do app

---
+++ gestao-festa/src/main/java/com/feitosa/gestaofesta/controller/ConvidadosController.java
@@ -0,0 +1,14 @@
+package com.feitosa.gestaofesta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConvidadosController {
+
+	@GetMapping("/convidados")
+	public String listar() {
+		return "listaConvidados";
+	}
+	
+}
