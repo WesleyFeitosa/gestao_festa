4b29354065d172d6ad4c89dffd9f4aef8737c59e
Wesley Feitosa <jwesleydasilva@gmail.com>
2019-04-21 15:43:49 -0300

    Integrando com Hibernate

--- gestao-festa/src/main/java/com/feitosa/gestaofesta/controller/ConvidadosController.java
+++ gestao-festa/src/main/java/com/feitosa/gestaofesta/controller/ConvidadosController.java
@@ -1,14 +1,23 @@
 package com.feitosa.gestaofesta.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.feitosa.gestaofesta.repository.Convidados;
 
 @Controller
 public class ConvidadosController {
 
+	@Autowired
+	Convidados convidados;
+	
 	@GetMapping("/convidados")
-	public String listar() {
-		return "listaConvidados";
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("listaConvidados");
+		mv.addObject("convidados", convidados.findAll());
+		return mv;
 	}
 	
 }
---
+++ gestao-festa/src/main/java/com/feitosa/gestaofesta/repository/Convidados.java
@@ -0,0 +1,9 @@
+package com.feitosa.gestaofesta.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.feitosa.gestaofesta.model.Convidado;
+
+public interface Convidados extends JpaRepository<Convidado, Long>{
+
+}
