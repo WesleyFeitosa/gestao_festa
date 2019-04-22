a30cdd633252076f41889971737a3eabb07e62ba
Wesley Feitosa <jwesleydasilva@gmail.com>
2019-04-22 12:17:26 -0300

    Melhorado o layout e adicionado Spring security

---
+++ gestao-festa/src/main/java/com/feitosa/gestaofesta/config/InMemorySecurityConfig.java
@@ -0,0 +1,19 @@
+package com.feitosa.gestaofesta.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+public class InMemorySecurityConfig {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception{
+		builder.inMemoryAuthentication()
+				.withUser("joao").password("{noop}123").roles("USER")
+				.and()
+				.withUser("wesley").password("{noop}123").roles("USER")
+				.and()
+				.withUser("maria").password("{noop}123").roles("USER");
+	}
+}
--- gestao-festa/src/main/java/com/feitosa/gestaofesta/controller/ConvidadosController.java
+++ gestao-festa/src/main/java/com/feitosa/gestaofesta/controller/ConvidadosController.java
@@ -3,21 +3,32 @@ package com.feitosa.gestaofesta.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.feitosa.gestaofesta.model.Convidado;
 import com.feitosa.gestaofesta.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
 	@Autowired
 	Convidados convidados;
 	
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("listaConvidados");
 		mv.addObject("convidados", convidados.findAll());
+		mv.addObject(new Convidado());
 		return mv;
+	}
+	
+	@PostMapping
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
 	}
 	
 }
